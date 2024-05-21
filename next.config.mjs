c79f411ffef7cb7c008b2f9b66584c93445af1a3
DebojyotiFloydee <debojyoti@floydee.com>
2024-05-21 17:37:25 +0530

    init

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,60 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     experimental: {
+//       appDir: true,
+//     },
+//     pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+//     webpack: (config, { isServer }) => {
+//       // Allow importing modules from src directory
+//       config.resolve.modules.push(__dirname + '/src');
+//       return config;
+//     },
+//   };
+  
+//   module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     pageExtensions: ['js'],
+//     webpack: (config, { isServer }) => {
+//       config.resolve.modules.push(__dirname + '/src');
+//       return config;
+//     },
+//   };
+  
+// //   module.exports = nextConfig;
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+
+// const withPWA = require('next-pwa')({
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+// });
+
+// const wpwa = withPWA({
+//     reactStrictMode: true,
+// });
+// export default wpwa;
+
+// import {  } from "next-pwa";
+
+// const nextPWA = withPWA;
+
+// const withPWA = require('next-pwa')({
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//   });
+  
+//   export default withPWA({
+//     reactStrictMode: true,
+//   });
